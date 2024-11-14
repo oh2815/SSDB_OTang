ab0c5ae79d13baec8225aaafb6abdd6cb1ab30d1
oh2815 <oh2815@naver.com>
2024-11-14 13:16:55 +0900

    wep page  실습

---
+++ 05_bootstrap/badminton.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+});
