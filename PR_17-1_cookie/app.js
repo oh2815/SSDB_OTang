c171c88359cf06b8b3936d4d98af70c15447813d
oh2815 <oh2815@naver.com>
2024-12-11 20:11:39 +0900

    2024 12 11 cookie, session 실습 및 JWT, encrypt 수업

--- PR_17-1_cookie/app.js
+++ PR_17-1_cookie/app.js
@@ -3,23 +3,24 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
-app.use(cookieParser());
+app.use(cookieParser("secret"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const cookieConfig = {
-  maxAge: 10 * 1000,
+  maxAge: 24 * 60 * 60 * 1000,
   httpOnly: true,
   signed: false,
 };
 
 // TODO: 쿠키 미들웨어 설정
 app.get("/", (req, res) => {
-  //   res.render("index");
   // TODO: 쿠키값 가져오기   및 index.ejs에 보내기
+  console.log("asd", req.cookies.prCookie);
   res.render("index", { popup: req.cookies.prCookie });
+  // 객체 선언해서 사용해도되고, res.cookie에서 요청할 때 바로 사용해도 되고.
 });
 
 app.post("/set-cookie", (req, res) => {
