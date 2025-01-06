97c07315050f9fcb365b27fbf6e988fde5a72204
oh2815 <oh2815@naver.com>
2025-01-06 17:56:18 +0900

    edit ) 25-01-06 react JSX 및 component 첫번째  수업 및  과제

--- 20-react-start/src/Pr_1.js
+++ 20-react-start/src/Pr_1.js
@@ -1,5 +1,5 @@
 export default function Pr_1() {
-  const name = "태원";
+  const name = "멍뭉";
   const animal = "강아지";
   const a = 8;
   const b = 5;
@@ -7,8 +7,8 @@ export default function Pr_1() {
   return (
     <div>
       <h2>
-        제 이름은 {name} 입니다.
-        {name}은 {animal}가 아닙니다.
+        제 반려동물의 이름은 {name} 입니다.
+        {name}은 {animal}입니다.
       </h2>
       <br />
       <span>{3 + 5 == 8 ? "정답입니다" : "오답입니다"}</span>
