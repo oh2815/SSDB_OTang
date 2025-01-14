3d3d1839d684c07d515dee3135398326480f7a90
oh2815 <oh2815@naver.com>
2025-01-15 02:48:17 +0900

    250114 RefClass 수업 리뷰

--- 22-ref-lifecycle/src/components/RefClass.jsx
+++ 22-ref-lifecycle/src/components/RefClass.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export class RefClass1 extends React.Component {
+  // 버튼에 onClick 걸어줌
   handleFocus = () => {
-    console.log(this.myInput);
-    console.log(this.myInput.value);
-    this.myInput.focus();
+    console.log(this.myInput); //input 태그 그 자체
+    console.log(this.myInput.value); // input안에 입력된 값
+    this.myInput.focus(); // ref에 focus를 줌
+    // 로그인시 유효성 검사가 잘 이루어지지 않았을 때 focus사용하면 client입장에서 봤을 때 좀 더 편리하다고 느낄 수있음
   };
   render() {
     return (
@@ -14,7 +16,7 @@ export class RefClass1 extends React.Component {
         <input
           type="text"
           ref={(ref) => {
-            this.myInput = ref; // input에 ref를 지정 myInput 이라는것은 임의 지정 this는 class.
+            this.myInput = ref; // input에 ref 콜백함수를 지정, myInput 이라는것은 임의 지정 this는 class.
           }}
         />
         {/* input창 안누르고 버튼눌러도  input창이 focus됨 */}
